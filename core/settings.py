e0312d1c51e8ae379eb2604e1af437cb3040a111
lumoratraid-adn <lumoratraid@gmail.com>
2026-02-14 17:15:50 +0530

    Remove venv and add gitignore

--- core/settings.py
+++ core/settings.py
@@ -28,6 +28,25 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
+
+import os
+import dj_database_url
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.environ.get("DEBUG") == "True"
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
+}
+
+
+
 # Application definition
 
 INSTALLED_APPS = [
